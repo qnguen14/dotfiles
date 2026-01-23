2207076a260cc5b375480d9b2c5f953ba4cc9888
nguyen <qnguyen.1014@gmail.com>
2026-01-23 22:56:42 +0700

    Update

--- .config/hypr/scripts/wallpaper-picker.sh
+++ .config/hypr/scripts/wallpaper-picker.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
-# === CONFIG ===
 WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
-SYMLINK_PATH="$HOME/.config/hypr/current_wallpaper"
+SYMLINK_PATH="$HOME/.config/hypr/current_wallpaper.jpg"
+CACHE_PATH="$HOME/.cache/current_wallpaper_thumb.jpg"
 
 cd "$WALLPAPER_DIR" || exit 1
 
-# === handle spaces name
 IFS=$'\n'
 
-# === ICON-PREVIEW SELECTION WITH ROFI, SORTED BY NEWEST ===
-SELECTED_WALL=$(for a in $(ls -t *.jpg *.png *.gif *.jpeg 2>/dev/null); do echo -en "$a\0icon\x1f$a\n"; done | rofi -dmenu -p ">")
+SELECTED_WALL=$(for a in $(ls -t *.jpg *.png *.gif *.jpeg *.mp4 *.mkv *.webm 2>/dev/null); do 
+    echo -en "$a\0icon\x1f$a\n"
+done | rofi -dmenu -p "Wallpaper >")
+
 [ -z "$SELECTED_WALL" ] && exit 1
 SELECTED_PATH="$WALLPAPER_DIR/$SELECTED_WALL"
+MIME_TYPE=$(file --mime-type -b "$SELECTED_PATH")
 
-# === SET WALLPAPER ===
-matugen image "$SELECTED_PATH"
+# === APPLY ===
 
-# === CREATE SYMLINK ===
-mkdir -p "$(dirname "$SYMLINK_PATH")"
-ln -sf "$SELECTED_PATH" "$SYMLINK_PATH"
+if [[ "$MIME_TYPE" == "video/"* ]]; then
+    ffmpeg -y -ss 00:00:05 -i "$SELECTED_PATH" -vframes 1 "$CACHE_PATH" > /dev/null 2>&1
+    matugen image "$CACHE_PATH"
+    pkill mpvpaper
+    mpvpaper -o "no-audio --loop" '*' "$SELECTED_PATH" & disown
+    mkdir -p "$(dirname "$SYMLINK_PATH")"
+    ln -sf "$CACHE_PATH" "$SYMLINK_PATH"
+
+else
+    pkill mpvpaper
+    matugen image "$SELECTED_PATH"
+    mkdir -p "$(dirname "$SYMLINK_PATH")"
+    ln -sf "$SELECTED_PATH" "$SYMLINK_PATH"
+fi
